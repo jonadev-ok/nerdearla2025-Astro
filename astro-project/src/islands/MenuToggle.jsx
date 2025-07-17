e2bbbf3fce15d3a41d9ae62b13829a4a893f344f
jonatan villalva <jvillalva.sistemas@gmail.com>
2025-07-16 23:10:26 -0300

    realizando ultimos retoques de header

--- astro-project/src/islands/MenuToggle.jsx
+++ astro-project/src/islands/MenuToggle.jsx
@@ -6,7 +6,7 @@ export default function MenuToggle() {
   return (
     <div className="md:hidden flex justify-end w-full relative">
       <button
-        className="text-xl text-white flex justify-between"
+        className="text-xl text-orange-500 flex justify-between"
         onClick={() => setOpen(!open)}
         aria-label="Abrir menú"
       >
@@ -15,10 +15,10 @@ export default function MenuToggle() {
       {open && (
         <div className="absolute right-0 mt-2 text-white shadow p-4 flex flex-col gap-4">
             
-          <a href="/" class="hover:text-cyan-500 "onClick={() => setOpen(false)}>Inicio</a>
-          <a href="#about" class="hover:text-cyan-500 "onClick={() => setOpen(false)}>Sobre Nosotros</a>
-          <a href="#services" class="hover:text-cyan-500 "onClick={() => setOpen(false)}>Servicios</a>
-          <a href="/Blog/" class="hover:text-cyan-500 "onClick={() => setOpen(false)}>Blog</a>
+          <a href="/" class=" hover:text-orange-500 "onClick={() => setOpen(false)}>Inicio</a>
+          <a href="#about" class="hover:text-orange-500 "onClick={() => setOpen(false)}>Sobre Nosotros</a>
+          <a href="#services" class="hover:text-orange-500 "onClick={() => setOpen(false)}>Servicios</a>
+          <a href="/Blog/" class="hover:text-orange-500 "onClick={() => setOpen(false)}>Blog</a>
         </div>
       )}
     </div>
