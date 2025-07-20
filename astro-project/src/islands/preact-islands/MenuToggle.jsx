89be8a405c94e15265cf65417d956e0f28af7dc7
jonatan villalva <jvillalva.sistemas@gmail.com>
2025-07-20 15:43:45 -0300

    separando imagenes estaticas por carpetas dependiendo del tipo y modificando rutas en componentes donde las estaba utilizando

--- astro-project/src/islands/preact-islands/MenuToggle.jsx
+++ astro-project/src/islands/preact-islands/MenuToggle.jsx
@@ -15,7 +15,7 @@ export default function MenuToggle() {
       {open && (
         <div className="absolute right-0 mt-15 text-white shadow p-4 flex flex-col gap-4">
             
-          <a href="/" class=" hover:text-orange-500 "onClick={() => setOpen(false)}>Inicio</a>
+          <a href="#home" class=" hover:text-orange-500 "onClick={() => setOpen(false)}>Inicio</a>
           <a href="#services" class="hover:text-orange-500 "onClick={() => setOpen(false)}>Servicios</a>
           <a href="#contact" class="hover:text-orange-500 "onClick={() => setOpen(false)}>Contacto</a>
           <a href="/Blog/" class="hover:text-orange-500 "onClick={() => setOpen(false)}>Blog</a>
