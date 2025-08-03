39f9241c6f9282821e2bc11e1673c49ba14ed0f5
jonatan villalva <jvillalva.sistemas@gmail.com>
2025-08-03 16:55:14 -0300

    creando botones de compartir articulo en redes

---
+++ astro-project/src/components/islands/ShareButtons.tsx
@@ -0,0 +1,53 @@
+/** @jsxImportSource preact */
+import { useState } from 'preact/hooks';
+import XspaceIcon from '../../icons/icons-socials/XspaceIcon.astro';
+import Xspace from "../../../public/logos/Xspace.svg"
+
+interface Props {
+  url: string;
+  title: string;
+}
+
+export default function ShareButtons({ url}: Props) {
+  const [copied, setCopied] = useState(false);
+
+  const twitterURL = `https://x.com/intent/tweet?url=${encodeURIComponent(url)}}`;
+  const linkedinURL = `https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(url)}`;
+
+  const copyLink = async () => {
+    try {
+      await navigator.clipboard.writeText(url);
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000);
+    } catch (err) {
+      console.error("No se pudo copiar el enlace", err);
+    }
+  };
+
+  return (
+    <div class="flex w-full justify-center gap-4 mt-4">
+      <a
+        href={twitterURL}
+        target="_blank"
+        rel="noopener noreferrer"
+        class="px-3 py-2 flex justify-around items-center  bg-orange-500 text-white rounded w-40"
+      >
+        <span>Compartir en</span> <img class="w-4" src="../../../public/logos/Xspace.svg"/>
+      </a>
+      <a
+        href={linkedinURL}
+        target="_blank"
+        rel="noopener noreferrer"
+        class="px-3 py-2 flex justify-around items-center  bg-orange-500 text-white rounded w-40"
+      >
+        <span>Compartir en</span> <img class="w-4" src="../../../public/logos/Linkedin.svg"/>
+      </a>
+      <button
+        onClick={copyLink}
+        class="px-3 py-2 flex justify-around items-center  bg-orange-500 text-white rounded w-40 cursor-pointer"
+      >
+        <span>{copied ? "¡Copiado!" : "Copiar enlace"}</span>
+      </button>
+    </div>
+  );
+}
