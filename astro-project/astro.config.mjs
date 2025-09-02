f51c17a0992a23f49f60ed618f88d6d8d21b1711
= <=>
2025-09-01 23:53:00 -0300

    trabajando en los errores para llevar a produccion

--- astro-project/astro.config.mjs
+++ astro-project/astro.config.mjs
@@ -7,13 +7,15 @@ import preact from '@astrojs/preact';
 
 import vercel from '@astrojs/vercel';
 
+
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
   },
 
   integrations: [preact()],
-  output: 'server',
-  adapter: vercel(),
 });
