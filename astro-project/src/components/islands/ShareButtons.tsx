18ff62ca3163194dd551af9d3c39ac05749ceef9
jonatan villalva <jvillalva.sistemas@gmail.com>
2025-09-07 23:14:15 -0300

    modificando urls de imagenes

--- astro-project/src/components/islands/ShareButtons.tsx
+++ astro-project/src/components/islands/ShareButtons.tsx
@@ -31,7 +31,7 @@ export default function ShareButtons({ url}: Props) {
         rel="noopener noreferrer"
         class="px-3 py-2 flex justify-around items-center  bg-blue-500 text-white rounded w-40"
       >
-        <span>Compartir en</span> <img class="w-4" src="../../../public/logos/Xspace.svg" alt="x-space" loading="lazy"/>
+        <span>Compartir en</span> <img class="w-4" src="/logos/Xspace.svg" alt="x-space" loading="lazy"/>
       </a>
       <a
         href={linkedinURL}
@@ -39,7 +39,7 @@ export default function ShareButtons({ url}: Props) {
         rel="noopener noreferrer"
         class="px-3 py-2 flex justify-around items-center  bg-blue-500 text-white rounded w-40"
       >
-        <span>Compartir en</span> <img class="w-4" src="../../../public/logos/Linkedin.svg" alt="linkedin" loading="lazy"/>
+        <span>Compartir en</span> <img class="w-4" src="/logos/Linkedin.svg" alt="linkedin" loading="lazy"/>
       </a>
       <button
         onClick={copyLink}
