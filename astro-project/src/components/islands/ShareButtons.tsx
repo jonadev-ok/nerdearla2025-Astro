cb1e339c2d3036ad00694192ac26fa4c2761242b
Jonatan Villalva <jvillalva.sistemas@gmail.com>
2025-09-08 22:33:36 -0300

    eliminando boton de compartir en x

--- astro-project/src/components/islands/ShareButtons.tsx
+++ astro-project/src/components/islands/ShareButtons.tsx
@@ -10,7 +10,6 @@ interface Props {
 export default function ShareButtons({ url}: Props) {
   const [copied, setCopied] = useState(false);
 
-  const twitterURL = `https://x.com/intent/post?url=${encodeURIComponent(url)}}`;
   const linkedinURL = `https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(url)}`;
 
   const copyLink = async () => {
@@ -25,14 +24,6 @@ export default function ShareButtons({ url}: Props) {
 
   return (
     <div class="flex w-full justify-center gap-4 mt-4">
-      <a
-        href={twitterURL}
-        target="_blank"
-        rel="noopener noreferrer"
-        class="px-3 py-2 flex justify-around items-center  bg-blue-500 text-white rounded w-40"
-      >
-        <span>Compartir en</span> <img class="w-4" src="/logos/Xspace.svg" alt="x-space" loading="lazy"/>
-      </a>
       <a
         href={linkedinURL}
         target="_blank"
