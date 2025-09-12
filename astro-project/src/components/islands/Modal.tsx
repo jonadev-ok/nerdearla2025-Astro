f9fd9d8d5ffccf0f2281286ec1ba10252b69f990
Jonatan Villalva <jvillalva.sistemas@gmail.com>
2025-09-12 20:21:10 -0300

    mnodificando estilos de modal

--- astro-project/src/components/islands/Modal.tsx
+++ astro-project/src/components/islands/Modal.tsx
@@ -27,24 +27,24 @@ export default function Modal({
 
   return (
     <div class="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
-      <div class="bg-white p-6 rounded-2xl shadow-lg max-w-sm text-center relative">
+      <div class="bg-black p-6 rounded-2xl shadow-lg max-w-sm text-center relative">
         {/* Botón cerrar */}
         <button
-          class="absolute top-2 right-2 text-gray-500 hover:text-black"
+          class="absolute top-2 right-2 text-white hover:text-red-500 cursor-pointer"
           onClick={() => setOpen(false)}
         >
           ✕
         </button>
 
-        <h2 class="text-lg font-semibold mb-4">{title}</h2>
-        <p class="text-gray-600 mb-4">{message}</p>
+        <h2 class="text-lg font-semibold mb-4 text-orange-500">{title}</h2>
+        <p class=" mb-4 text-white">{message}</p>
 
         {/* Botón de descarga */}
         <a
           href={downloadUrl}
           target="_blank"
           download
-          class="inline-block bg-blue-600 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-700 transition"
+          class="inline-block bg-orange-500 text-white px-4 py-2 rounded-lg shadow hover:bg-orange-700 transition"
         >
           {buttonText}
         </a>
