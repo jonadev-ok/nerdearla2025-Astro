89e8cdfe74a8e28bfd10efa00a86d6115c4e6765
jonatan villalva <jvillalva.sistemas@gmail.com>
2026-03-05 14:17:49 -0300

    chore(config): add navigation.ts

---
+++ astro-project/src/config/navigation.ts
@@ -0,0 +1,6 @@
+export const navItems = [
+  { label: "Inicio", href: "/" },
+  { label: "Servicios", href: "/#servicios" },
+{ label: "Proyectos", href: "/#proyectos" },
+  { label: "Blog", href: "/blog" },
+];
